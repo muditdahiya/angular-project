64113b77bbe67de2af702c5a76f2fa941f3aa1ba
muditdahiya <mdtheroxtar@gmail.com>
2023-04-03 12:31:49 -0400

    bootstrap

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,78 @@
-import { TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRouteSnapshot, ChildActivationEnd, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { RouterStub } from '@testing/stubs';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+    let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    let component: AppComponent;
+    let componentDE: DebugElement;
+    let componentNE: Element;
 
-  it(`should have as title 'project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('project');
-  });
+    let router: RouterStub;
+    let titleService: Title;
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project app is running!');
-  });
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
+            providers: [
+                {
+                    provide: Router,
+                    useValue: new RouterStub(),
+                },
+                Title,
+            ],
+            declarations: [AppComponent],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(AppComponent);
+
+        componentDE = fixture.debugElement.children[0];
+        component = componentDE.componentInstance;
+        componentNE = componentDE.nativeElement;
+
+        fixture.detectChanges();
+
+        router = (TestBed.inject(Router) as unknown) as RouterStub;
+        titleService = TestBed.inject(Title);
+    }));
+
+    it('should create the app', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it(`should have a initial title 'sb-clean-blog-angular'`, () => {
+        expect(component.title).toEqual('sb-clean-blog-angular');
+    });
+
+    it('should set the default title', () => {
+        spyOn(titleService, 'setTitle');
+        router.events.next(
+            new ChildActivationEnd(({
+                firstChild: {
+                    firstChild: null,
+                    data: {},
+                },
+            } as unknown) as ActivatedRouteSnapshot)
+        );
+        expect(titleService.setTitle).toHaveBeenCalledTimes(1);
+    });
+
+    it('should set the title from data', () => {
+        spyOn(titleService, 'setTitle');
+        router.events.next(
+            new ChildActivationEnd(({
+                firstChild: null,
+                data: {
+                    title: 'TEST',
+                },
+            } as unknown) as ActivatedRouteSnapshot)
+        );
+        expect(titleService.setTitle).toHaveBeenCalledWith('TEST');
+    });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,21 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppCommonModule } from '@common/app-common.module';
+import { NavigationModule } from '@modules/navigation/navigation.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { AddPostPageComponent } from './add-post-page/add-post-page.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
-import { FavouritesPageComponent } from './favourites-page/favourites-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { MyPostsPageComponent } from './my-posts-page/my-posts-page.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
-import { SingupPageComponent } from './singup-page/singup-page.component';
-import { PostComponent } from './components/post/post.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomePageComponent,
-    AboutPageComponent,
-    AddPostPageComponent,
-    ContactPageComponent,
-    FavouritesPageComponent,
-    LoginPageComponent,
-    MyPostsPageComponent,
-    ProfilePageComponent,
-    SingupPageComponent,
-    PostComponent,
-    NavigationComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        AppCommonModule.forRoot(),
+        NavigationModule.forRoot(),
+    ],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
