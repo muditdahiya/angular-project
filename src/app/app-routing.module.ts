58e02ab6bebe29a325f4e77f610f45a46c6725d3
unsafemango <unsafemango@gmail.com>
2023-04-04 16:10:39 -0400

    added deleted data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SingupPageComponent } from './singup-page/singup-page.component'
+import { SingupPageComponent } from './singup-page/singup-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: HomePageComponent },
   { path: 'signup', component: SingupPageComponent },
   { path: 'login', component: LoginPageComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
