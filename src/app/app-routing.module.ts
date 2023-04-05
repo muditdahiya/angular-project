8454e4fb410e33a84886976a678428e37955ed25
muditdahiya <mdtheroxtar@gmail.com>
2023-04-05 01:40:37 -0400

    made all routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SingupPageComponent } from './singup-page/singup-page.component'
+import { SingupPageComponent } from './singup-page/singup-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyPostsPageComponent } from './my-posts-page/my-posts-page.component';
+import { FavouritesPageComponent } from './favourites-page/favourites-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 const routes: Routes = [
-  { path: '', component:HomePageComponent },
+  { path: '', component: HomePageComponent },
   { path: 'signup', component: SingupPageComponent },
   { path: 'login', component: LoginPageComponent },
-  {path:'contactus',component:ContactPageComponent},
+  { path: 'myposts', component: MyPostsPageComponent },
+  { path: 'favourites', component: FavouritesPageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'contactus', component: ContactPageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
