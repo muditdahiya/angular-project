284d10b5b749817fdeddb3054499b7988af7517c
Srikruthi23 <srikruthinallavelli23@gmail.com>
2023-04-17 01:57:07 -0400

    login&contactus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LogoutComponent } from './logout/logout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SingupPageComponent } from './singup-page/singup-page.component';
@@ -8,16 +9,20 @@ import { MyPostsPageComponent } from './my-posts-page/my-posts-page.component';
 import { FavouritesPageComponent } from './favourites-page/favourites-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './auth.guard';
+
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: '', component: HomePageComponent ,canActivate:[AuthGuard]},
   { path: 'signup', component: SingupPageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'myposts', component: MyPostsPageComponent },
-  { path: 'favourites', component: FavouritesPageComponent },
+  { path: 'myposts', component: MyPostsPageComponent ,canActivate:[AuthGuard]},
+  { path: 'favourites', component: FavouritesPageComponent ,canActivate:[AuthGuard]},
   { path: 'profile', component: ProfilePageComponent },
   { path: 'about', component: AboutPageComponent },
   { path: 'contactus', component: ContactPageComponent },
+  {path:'logout',component:LogoutComponent}
 ];
 
 @NgModule({
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap,catchError } from 'rxjs';
+import { IAuth } from '../interfaces/IAuth';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _isLoggedIn$ = new BehaviorSubject<boolean>(false);
+  isLoggedIn$ = this._isLoggedIn$.asObservable();
+
+  private url: string = 'http://muditdahiya.com/web-backend/login';
+
+  constructor(private http: HttpClient) {
+    const mytoken = localStorage.getItem('authToken');
+    this._isLoggedIn$.next(!!mytoken);
+  }
+  login(email: string, password: string): Observable<IAuth> {
+    return this.http
+      .post<IAuth>(this.url, {
+        email: email,
+        password: password,
+      })
+      .pipe(
+        tap((response: any) => {
+            localStorage.setItem('authToken', response.token);
+            this._isLoggedIn$.next(true);
+        })
+      );
+  }
+  logout() {
+    this._isLoggedIn$.next(false);
+    localStorage.removeItem('authToken');
+  }
+  
+  mytoken = localStorage.getItem('authToken');
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-auth-token': this.mytoken!,
+    }),
+  };
+
+}
