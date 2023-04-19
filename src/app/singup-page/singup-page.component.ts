247f420c6afb9193df6cb0d73709892c3de23901
muditdahiya <mdtheroxtar@gmail.com>
2023-04-19 14:40:54 -0400

    fonal submission

--- src/app/singup-page/singup-page.component.ts
+++ src/app/singup-page/singup-page.component.ts
@@ -1,37 +1,45 @@
 import { BackendServiceService } from './../services/backend-service.service';
-import { Component ,OnInit} from '@angular/core';
-import { FormGroup,FormControl,Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Isignup } from '../interfaces/Isignup';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-singup-page',
   templateUrl: './singup-page.component.html',
-  styleUrls: ['./singup-page.component.css']
+  styleUrls: ['./singup-page.component.css'],
 })
 export class SingupPageComponent {
-title='SIGNUP';
+  title = 'SIGNUP';
 
-signupForm  = new FormGroup({
-  fname:new FormControl('', Validators.required),
-  lname:new FormControl('', Validators.required),
-  email: new FormControl('', [Validators.required, Validators.email]),
-  password: new FormControl('', Validators.required),
-
-});
-constructor(private BackendServiceService: BackendServiceService) {}
-onSubmit() {
-  const data:Isignup[]=[{
-    fname:this.signupForm.value.fname!,
-    lname:this.signupForm.value.lname!,
-    email:this.signupForm.value.email!,
-    password:this.signupForm.value.password!,
-  }];
-  this.BackendServiceService.signup(data)
-  .subscribe(response => {
-    console.log(response); 
-    this.signupForm.reset();
+  signupForm = new FormGroup({
+    fname: new FormControl('', Validators.required),
+    lname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
   });
+  constructor(
+    private BackendServiceService: BackendServiceService,
+    private router: Router
+  ) {}
+  onSubmit() {
+    const data: Isignup[] = [
+      {
+        fname: this.signupForm.value.fname!,
+        lname: this.signupForm.value.lname!,
+        email: this.signupForm.value.email!,
+        password: this.signupForm.value.password!,
+      },
+    ];
+    this.BackendServiceService.signup(data).subscribe(
+      (response) => {
+        console.log(response);
+        this.signupForm.reset();
+      },
+      (error) => {
+        this.router.navigateByUrl('/');
+        this.signupForm.reset();
+      }
+    );
+  }
 }
-}
-
